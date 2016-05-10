f51681bc91547b2c7c70b78a02d0082f53eccda4
leexm <807415781@qq.com>
2016-05-10 20:37:42 +0800

    修改demo

--- src/main/java/velocity/HelloVelocity.java
+++ src/main/java/velocity/HelloVelocity.java
@@ -12,9 +12,10 @@
 package velocity;
 
 import java.io.StringWriter;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Properties;
+import java.util.Date;
+import java.util.List;
 
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
@@ -33,59 +34,26 @@ public class HelloVelocity {
 
     public static void main(String[] args) {
         // 初始化模板引擎
-        Properties props = new Properties();
-        props.put(RuntimeConstants.RESOURCE_LOADER, "classpath");
-        props.put("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
-        VelocityEngine ve = new VelocityEngine(props);
-//        VelocityEngine ve = new VelocityEngine();
-//        ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
-//        ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+        VelocityEngine ve = new VelocityEngine();
+        ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
+        ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+        ve.init();
         
-//        ve.init();
-        
-        Template t = ve.getTemplate("frm.vm");
+        Template template = ve.getTemplate("hellovelocity.vm");
         VelocityContext ctx = new VelocityContext();
         
-//        ctx.put("name", "leexm");
-//        ctx.put("date", (new Date()).toString());
-//        
-//        List<String> temp = new ArrayList<String>();
-//        temp.add("1");
-//        temp.add("2");
-//        ctx.put("list", temp);
+        ctx.put("name", "leexm");
         
-        ctx.put("action", "./submit");
-        ArrayList<HashMap<String, String>> inputs = new ArrayList<HashMap<String,String>>();
-        HashMap<String, String> input1 = new HashMap<String, String>();
-        input1.put("id", "title");
-        input1.put("title", "标题：");
-        inputs.add(input1);
-        HashMap<String, String> input2 = new HashMap<String, String>();
-        input2.put("id", "brief");
-        input2.put("title", "摘要：");
-        inputs.add(input2);
-        ctx.put("inputs", inputs);
-        ArrayList<HashMap<String, Object>> selects = new ArrayList<HashMap<String,Object>>();
-        HashMap<String, Object> select1 = new HashMap<String, Object>();
-        selects.add(select1);
-        select1.put("id", "sex");
-        select1.put("title", "性别：");
-        HashMap<Integer, String> kv1 = new HashMap<Integer, String>();
-        kv1.put(0, "男");
-        kv1.put(1, "女");
-        select1.put("items", kv1);
-        HashMap<String, Object> select2 = new HashMap<String, Object>();
-        selects.add(select2);
-        select2.put("id", "job");
-        select2.put("title", "职业：");
-        HashMap<Integer, String> kv2 = new HashMap<Integer, String>();
-        kv2.put(0, "Java工程师");
-        kv2.put(1, "Net工程师");
-        select2.put("items", kv2);
-        ctx.put("selects", selects);
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        ctx.put("date", formatter.format(new Date()));
+        
+        List<String> temp = new ArrayList<String>();
+        temp.add("1");
+        temp.add("2");
+        ctx.put("list", temp);
         
         StringWriter sw = new StringWriter();
-        t.merge(ctx, sw);
+        template.merge(ctx, sw);
         System.out.println(sw.toString());
     }
 
