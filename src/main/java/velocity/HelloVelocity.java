6f97655f648de6e1cb98ee2f7ac8169f11a24e91
leexm <807415781@qq.com>
2016-05-11 13:39:51 +0800

    重定义模板的位置

--- src/main/java/velocity/HelloVelocity.java
+++ src/main/java/velocity/HelloVelocity.java
@@ -5,12 +5,12 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Properties;
 
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
-import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.app.Velocity;
 import org.apache.velocity.runtime.RuntimeConstants;
-import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 
 /**
  * @type HelloVelocity
@@ -23,15 +23,27 @@ public class HelloVelocity {
 
     public static void main(String[] args) {
         // 初始化模板引擎
-        VelocityEngine ve = new VelocityEngine();
-        ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
-        ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
-        ve.init();
+        // 初始化方法一
+//        VelocityEngine ve = new VelocityEngine();
+//        ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
+//        ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+//        ve.init();
+//        Template template = ve.getTemplate("hellovelocity.vm");
         
-        Template template = ve.getTemplate("hellovelocity.vm");
+        // 初始化方法二
+        Properties p = new Properties();
+        //加载classpath目录下的vm文件
+//        p.put(RuntimeConstants.RESOURCE_LOADER, "classpath");
+//        p.put("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+        
+        //根据绝对路径加载，vm文件置于硬盘某分区中
+        p.put(RuntimeConstants.FILE_RESOURCE_LOADER_PATH, "/Users/leexm/documents/workspace/hello-velocity/src/main/resources");
+        
+        Velocity.init(p);
+        Template template = Velocity.getTemplate("hellovelocity.vm");
+        
         VelocityContext ctx = new VelocityContext();
-        
-        ctx.put("name", "leexm");
+        ctx.put("name", "比克大魔王");
         
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
         ctx.put("date", formatter.format(new Date()));
--- src/main/java/velocity/HelloVelocity2.java
+++ src/main/java/velocity/HelloVelocity2.java
@@ -3,52 +3,52 @@ package velocity;
 import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.runtime.RuntimeConstants;
-import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 
 public class HelloVelocity2 {
 
     public static void main(String[] args) {
         // 初始化模板引擎
         Properties props = new Properties();
-        props.put(RuntimeConstants.RESOURCE_LOADER, "classpath");
-        props.put("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+        props.put(RuntimeConstants.FILE_RESOURCE_LOADER_PATH, "/Users/leexm/documents/workspace/hello-velocity/src/main/resources");
         VelocityEngine ve = new VelocityEngine(props);
         ve.init();
         // 配置引擎上下文对象
         VelocityContext ctx = new VelocityContext();
         ctx.put("action", "./submit");
         
-        ArrayList<HashMap<String, String>> inputs = new ArrayList<HashMap<String,String>>();
-        HashMap<String, String> input1 = new HashMap<String, String>();
+        List<Map<String, String>> inputs = new ArrayList<Map<String,String>>();
+        Map<String, String> input1 = new HashMap<String, String>();
         input1.put("id", "title");
         input1.put("title", "标题：");
         inputs.add(input1);
-        HashMap<String, String> input2 = new HashMap<String, String>();
+        Map<String, String> input2 = new HashMap<String, String>();
         input2.put("id", "brief");
         input2.put("title", "摘要：");
         inputs.add(input2);
         ctx.put("inputs", inputs);
         
-        ArrayList<HashMap<String, Object>> selects = new ArrayList<HashMap<String,Object>>();
-        HashMap<String, Object> select1 = new HashMap<String, Object>();
+        List<Map<String, Object>> selects = new ArrayList<Map<String,Object>>();
+        Map<String, Object> select1 = new HashMap<String, Object>();
         selects.add(select1);
         select1.put("id", "sex");
         select1.put("title", "性别：");
-        HashMap<Integer, String> kv1 = new HashMap<Integer, String>();
+        Map<Integer, String> kv1 = new HashMap<Integer, String>();
         kv1.put(0, "男");
         kv1.put(1, "女");
         select1.put("items", kv1);
-        HashMap<String, Object> select2 = new HashMap<String, Object>();
+        Map<String, Object> select2 = new HashMap<String, Object>();
         selects.add(select2);
         select2.put("id", "job");
         select2.put("title", "职业：");
-        HashMap<Integer, String> kv2 = new HashMap<Integer, String>();
+        Map<Integer, String> kv2 = new HashMap<Integer, String>();
         kv2.put(0, "Java工程师");
         kv2.put(1, "Net工程师");
         select2.put("items", kv2);
@@ -59,7 +59,8 @@ public class HelloVelocity2 {
         StringWriter sw = new StringWriter();
         // 渲染模板
         template.merge(ctx, sw);
-        System.out.print(sw.toString());
+        System.out.println(sw.toString());
+        System.out.println(ctx.get("selects"));
     }
 
 }
